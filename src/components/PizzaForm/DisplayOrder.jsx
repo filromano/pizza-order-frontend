73ffbdc3073cabd8d80e70a9eb610c815f1b2ba5
Filipe Romano <filromano@gmail.com>
2020-07-12 20:27:43 -0300

    filter

--- src/components/PizzaForm/DisplayOrder.jsx
+++ src/components/PizzaForm/DisplayOrder.jsx
@@ -3,16 +3,26 @@ import '../../style/DisplayOrder.css';
 import React from 'react';
 
 export default (props) => {
+
+  let flavor2;
+
+  if(props.order.flavor2) {
+    flavor2 = 
+    <div>
+        <p>Sabor 2: {props.order.flavor2}</p>
+        <p>Preço: R${props.order.price2.toFixed(2).replace('.', ',')}</p>
+    </div>
+  }
+
   return (
     <div className="DisplayOrder">
       <h3>Suas escolhas: (pizza atual)</h3>
       <p>Massa: {props.order.crust}</p>
       <p>Tamanho: {props.order.size}</p>
-      <p>Sabor: {props.order.flavor1}</p>
-      <p>Preço: {props.order.price1}</p>
-      <p>Sabor: {props.order.flavor2}</p>
-      <p>Preço: {props.order.price2}</p>
-      <p>Total: {props.order.total}</p>
+      <p>Sabor 1: {props.order.flavor1}</p>
+      <p>Preço: R${props.order.price1.toFixed(2).replace('.', ',')}</p>
+      {flavor2}
+      <p>Total: R${props.order.total.toFixed(2).replace('.', ',')}</p>
     </div>
   )
 };
