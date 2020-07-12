e56f6a2b70daaa4ee937996d4390912e27be540d
Filipe Romano <filromano@gmail.com>
2020-07-12 10:38:31 -0300

    fix

--- src/components/PizzaForm/Checkout.jsx
+++ src/components/PizzaForm/Checkout.jsx
@@ -6,7 +6,6 @@ export default (props) => {
   return(
     <div className="Checkout">
       <h2>Obrigado por nos escolher</h2>
-      <button>Adicionar uma pizza</button>
       <button>Finalizar pedido</button>
       <button>Recomeçar pedido</button>
     </div>
